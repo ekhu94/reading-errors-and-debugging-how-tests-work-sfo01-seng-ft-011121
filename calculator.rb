ff11734f2c5f2c592f74b61b4bb9d1263323800d
Erik Huang <erikhuangdrums@gmail.com>
2020-12-16 22:11:39 +0000

    Automatically backed up by Learn

--- calculator.rb
+++
@@ -1,6 +0,0 @@
-first_number = 8008
-second_number = 72077
-sum = first_number + second_number
-difference = first_number - second_number
-product = first_number * second_number
-quotient = first_number / second_number
